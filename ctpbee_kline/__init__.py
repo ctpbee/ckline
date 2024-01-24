840207079078c4f5a0cd3fd1c54094e8a9436fd6
somewheve <somewheve@gmail.com>
2024-01-24 21:03:24 +0800

    set this default to 1min kline

--- ctpbee_kline/__init__.py
+++ ctpbee_kline/__init__.py
@@ -1,31 +1,30 @@
 from ctpbee import Tool
-from ctpbee.constant import ToolRegisterType, TickData
+from ctpbee.constant import ToolRegisterType, TickData, Interval
 from ctpbee.level import tool_register
 from ctpbee.constant import BarData
 
 
 class Kline(Tool):
-    def __init__(self, level):
+    def __init__(self):
         """
         :param level:
         """
-        super().__init__(level)
+        super().__init__("kline")
         self.bars = {}
         self.init = False
-        if type(level) != list:
-            raise TypeError("请传入k线支持级别列表 如果支持1min, 3分钟请使用level=[1,3]传入参数")
-        self.level: list = level
 
     @tool_register(ToolRegisterType.TICK)
     def on_tick(self, tick: TickData):
         if not self.init:
-            for strategy in self._app._extensions.values():
+            for strategy in self.app._extensions.values():
                 self.app.tools[self.name].add_func(func=strategy.on_bar, r_type=ToolRegisterType.TICK)
+            self.init = True
         if self.bars.get(tick.local_symbol, None):
             if tick.datetime.minute != self.bars[tick.local_symbol][0].minute:
                 bar = BarData(
                     symbol=tick.symbol,
                     exchange=tick.exchange,
+                    interval=1,
                     datetime=self.bars[tick.local_symbol][0],
                     volume=tick.volume - self.bars[tick.local_symbol][5],
                     high_price=self.bars[tick.local_symbol][1],
@@ -36,7 +35,6 @@ class Kline(Tool):
                 self.bars[tick.local_symbol] = [tick.datetime, tick.last_price, tick.last_price,
                                                 tick.last_price, tick.last_price, tick.volume]
                 return bar
-
             # high
             self.bars[tick.local_symbol][1] = max(self.bars[tick.local_symbol][1], tick.last_price)
             # low
